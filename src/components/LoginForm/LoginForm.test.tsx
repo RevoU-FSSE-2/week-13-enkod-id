cd52461b31223ae7cb16b4c88f977aab1ce6fc8b
Ari P <ari.putra@runchise.com>
2023-09-26 19:10:25 +0700

    add login form unit testing

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,47 +1,49 @@
-import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react'
+import { waitFor, fireEvent, render, screen } from '@testing-library/react'
 import LoginForm from '.';
 
 describe('test login form', () => {
     const mockProps = jest.fn();
-    it('title render correctly', async () => {
+    test('title render correctly', async () => {
         render(<LoginForm onSubmit={mockProps}/>)
         const title = screen.getByText('Login Page')
         expect(title).toBeDefined();
     })
 
-    it('label username render correctly', async () => {
+    test('label username render correctly', async () => {
         render(<LoginForm onSubmit={mockProps}/>)
         const title = screen.getByText('Username')
         expect(title).toBeDefined();
     })
 
-    it('label password render correctly', async () => {
+    test('label password render correctly', async () => {
         render(<LoginForm onSubmit={mockProps}/>)
         const title = screen.getByText('Password')
         expect(title).toBeDefined();
     })
 
-    it('button submit render correctly', async () => {
+    test('button submit render correctly', async () => {
         render(<LoginForm onSubmit={mockProps}/>)
         const title = screen.getByText('Submit')
         expect(title).toBeDefined();
     })
 
-    // it('onSubmit works correctly', async () => {
-    //     render(<LoginForm onSubmit={mockProps}/>)
-    //     const usernameInput = screen.getByPlaceholderText('Enter username');
-    //     const passwordInput = screen.getByPlaceholderText('Enter password');
-    //     const submitButton = screen.getByText('Submit')
+    test('onSubmit works correctly', async () => {
+        const { getByPlaceholderText, getByText } = render(<LoginForm onSubmit={mockProps} />);
+        const usernameInput = getByPlaceholderText('Enter username') as HTMLInputElement;
+        const passwordInput = getByPlaceholderText('Enter password') as HTMLInputElement;
+        const submitButton = getByText('Submit');
 
-    //     fireEvent.change(usernameInput, { target: { value: 'testuser'}})
-    //     fireEvent.change(passwordInput, { target: { value: 'testpassword'}})
+        fireEvent.change(usernameInput, { target: { value: 'testuser' } });
+        fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
 
-    //     fireEvent.click(submitButton)
+        fireEvent.click(submitButton);
 
-    //     expect(usernameInput).toHaveBeenCalledWith({
-    //         username: 'testuser',
-    //         password: 'testpassword'
-    //     })
-    // })
+        await waitFor(() => {
+            expect(mockProps).toHaveBeenCalledTimes(1);
+            expect(mockProps).toHaveBeenCalledWith({
+                username: 'testuser',
+                password: 'testpassword',
+            });
+        });
+    });
 })
